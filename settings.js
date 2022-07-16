0bfd2cb24fea02489673db61cfb79b83f3f2ae22
Deckard12x <109419946+Deckard12x@users.noreply.github.com>
2022-07-16 07:51:17 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -38,7 +38,7 @@ global.APIKeys = {
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
+global.vcardowner = ['51994299226'] //ur owner number
+global.ownername = "🥷🏻 ᴅᴇᴄᴋᴀʀᴅ  🥷🏻" //ur owner name
+global.ytname = "FB: Noel" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Lima, Perú" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['51994299226']
+global.ownertag = '51994299226' //ur tag number
+global.botname = '🥷🏻 ᴅᴇᴄᴋᴀʀᴅ | ʙᴏᴛ 🥷🏻' //ur bot name
+global.linkz = "https://www.facebook.com/Deckardx12" //your theme url which will be displayed on whatsapp
+global.websitex = "https://wa.me/51994299226" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "🥷🏻" //ur menu react emoji
+global.themeemoji = "🪙" //ur theme emoji
+global.packname = "DECKARD\n\n\n\n\n\n\n\n\n\n\n🥷🏻🥷🏻🥷🏻 " //ur sticker watermark packname
+global.author = "+51 994 299 226" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['+51994299226'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -105,20 +105,20 @@ global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    success: 'Hecho ✓',
+    admin: '*[❗] ESTA FUNCIÓN ES SOLO PARA ADMINISTRADORES!*',
+    botAdmin: '*[❗] El BOT DEBE SER ADMINISTRADOR PRIMERO!*',
+    owner: '*[❗]* ESTA FUNCIÓN ES SOLO PARA *DECKARD!*',
+    group: '*[❗] SOLO FUNCIONA EN GRUPOS!*',
+    private: '*[❗] FUNCIONES UTILIZADAS SOLO PARA CHAT PRIVADO!*',
+    bot: '*[❗] ESTA FUNCIÓN SOLO ES PARA EL BOT*',
+    wait: '[ ! ] EN PROCESO...',
+    linkm: '*[❗] ¿DÓNDE ESTA EL LINK? 🤨*',
+    error: '*ERROR!!*',
+    endLimit: '*[❕] SU LÍMITE DIARIO HA EXPIRADO, EL LÍMITE SE RESTABLECE CADA 12 HORAS.*',
+    ban: '_[❕] HAS SIDO BANEADO POR *DECKARD.* PARA VOLVER A USAR EL BOT, CHATEA CON MI CREADOR._',
+    nsfw: '*[❕] LA FUNCIÓN NSFW NO SE HA ACTIVADO, COMUNÍQUESE CON EL ADMINISTRADOR PARA ACTIVARLA.*',
+    banChat: '_[❗] EL BOT FUE BANEADO EN ESTE GRUPO, COMUNÍQUESE CON *DECKARD* PARA DESBANEAR EL GRUPO._'
 }
     global.limitawal = {
     premium: "Infinity",
